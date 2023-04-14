ce0f977c5a6a541545795711cc3e72cd87fa4f89
dixonhuang10 <107824769+dixonhuang10@users.noreply.github.com>
2023-04-14 12:26:01 -0500

    Add files via upload

---
+++ Elemental War.py
@@ -0,0 +1,95 @@
+import random
+
+user_wins = 0
+draws = 0
+computer_wins = 0
+
+options = ["earth", "fire", "metal", "water", "wood"]
+
+
+while True: 
+
+    
+    user_input = input("Type Earth/Fire/Metal/Water/Wood or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options: 
+        continue 
+
+    random_number = random.randint(0, 4)
+    # earth: 0, fire: 1, metal: 2, water: 3, wood:4
+    computer_pick = options[random_number]
+    print("Computer Picked", computer_pick + ".")
+
+    if user_input == "water" and computer_pick == "metal":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "metal" and computer_pick == "earth":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "earth" and computer_pick == "fire":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "fire" and computer_pick == "wood":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "wood" and computer_pick == "water":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "water" and computer_pick == "fire":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "fire" and computer_pick == "metal":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "metal" and computer_pick == "wood":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "wood" and computer_pick == "earth":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "earth" and computer_pick == "water":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "water" and computer_pick == "water":
+        print("Draw!")
+        draws += 1
+
+    elif user_input == "fire" and computer_pick == "fire":
+        print("Draw!")
+        draws += 1
+
+    elif user_input == "metal" and computer_pick == "metal":
+        print("Draw!")
+        draws += 1
+
+    elif user_input == "wood" and computer_pick == "wood":
+        print("Draw!")
+        draws += 1
+
+    elif user_input == "earth" and computer_pick == "earth":
+        print("Draw!")
+        draws += 1
+
+    else: 
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("Draws", draws)
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
+
+
+
